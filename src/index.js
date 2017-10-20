581e735f32cfb4747989dcd4775b97d18ef1ccc5
Vladimir Klepov <v.klepov@gmail.com>
2017-10-20 23:39:28 +0300

    add some code & build

---
+++ src/index.js
@@ -0,0 +1,74 @@
+import { Component, createElement } from 'react';
+import PropTypes from 'prop-types';
+import mitt from 'mitt';
+
+
+function makeBusKey(eventName) {
+  return 'redioBus' + eventName;
+}
+
+
+function emitter(eventName, emitterProp) {
+  return function decorateEmitter(PlainComponent) {
+    class Emitter extends Component {
+      constructor(p, c) {
+        super(p, c);
+        this.bus = this.context[makeBusKey(eventName)] || mitt();
+        this.emit = payload => this.bus.emit(eventName, payload);
+      }
+
+      getChildContext() {
+        return { [makeBusKey(eventName)]: this.bus };
+      }
+
+      render() {
+        const props = Object.assign({}, this.props);
+        props[emitterProp] = this.emit;
+        return createElement(PlainComponent, propsWithEmit);
+      }
+    }
+
+    Emitter.contextTypes = contextTypes = {
+      [makeBusKey(eventName)]: PropTypes.any,
+    };
+
+    return Emitter;
+  };
+}
+
+function receiver(eventName, handle) {
+  return function decorateReceiver(PlainComponent) {
+    class Receiver extends Component {
+      constructor(p, c) {
+        super(p, c);
+        this.handle = payload => handle(payload, this.props);
+      }
+
+      componentWillMount() {
+        const bus = this.context[makeBusKey(eventName)];
+        if (!bus) return;
+        bus.on(eventName, this.handle);
+      }
+
+      componentWillUnmount() {
+        const bus = this.context[makeBusKey(eventName)];
+        if (!bus) return;
+        bus.off(eventName, this.handle);
+      }
+
+      render() {
+        return createElement(PlainComponent, this.props);
+      }
+    }
+
+    Receiver.contextTypes = contextTypes = {
+      [makeBusKey(eventName)]: PropTypes.any,
+    };
+
+    return Receiver;
+  };
+}
+
+
+export default { emitter,  receiver };
+export { emitter, receiver };
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: { index: './src/index.js' },
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: "[name].js",
+      library: "redio",
+      libraryTarget: 'umd',
+      umdNamedDefine: true
+    },
+    externals: {
+      'react': 'commonjs react',
+      'prop-types': 'commonjs prop-types'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env']
+            }
+          }
+        }
+      ]
+    }
+}
